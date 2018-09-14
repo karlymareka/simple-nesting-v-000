b82541a8f2949b48f5f2e3eab940689335ab1b8e
Karly Mareka <karly.mareka@gmail.com>
2018-09-14 15:11:48 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
- programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
- programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+#a[:"0"][:NY] = {:count => 11}
+ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
+ programmer_hash[:yukihiro_matsumoto] = {:languages => ["LISP", "C"]
 end
 
 def changing_alan
