a1a18b4ce662a8f66665168b183e97c07cc2e180
Karly Mareka <karly.mareka@gmail.com>
2018-09-14 15:04:45 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,8 @@ def adding_matz
         }
      }
 
- programmer_hash << [:yukihiro_matsumoto] = :known_for => "Ruby"   
+ programmer_hash << [:yukihiro_matsumoto] = :known_for => "Ruby"
+ programmer_hash
 end
 
 def changing_alan
